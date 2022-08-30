15ace10005ef005fd14809904ab2e61f0dd3c60d
Ranjeet <64788112+ranjeet-warrior@users.noreply.github.com>
2022-08-30 04:27:52 +0000

    Update App.js

--- assignment5/src/App.js
+++ assignment5/src/App.js
@@ -9,6 +9,26 @@ function App() {
           <li>A higher-order component(HOC) is an advanced technique in React for reusing component logic</li>
           <li>HOCs are not part of React API,They are a pattern that emerges from React's compositional nature</li>
           <li>a higher-order component is a function that takes a component and returns a new component</li>
+       // Example of Higher Order Functions :-
+                  // "GlobalDataSource" is some global data source class ArticlesList extends React.Component {
+                   constructor(props) {
+                   super(props);
+                 this.handleChange = this.handleChange.bind(this); this.state = {
+                 articles: GlobalDataSource.getArticles(), };
+                 } componentDidMount() {
+                 // Listens to the changes added
+                GlobalDataSource.addChangeListener(this.handleChange); }
+                componentWillUnmount() {
+      // Listens to the changes removed GlobalDataSource.removeChangeListener(this.handleChange);
+               } handleChange() {
+     // States gets Update whenver data source changes this.setState({
+               articles: GlobalDataSource.getArticles(), });
+               } render() {
+               return ( <div>
+            {this.state.articles.map((article) => ( <ArticleData article={article} key={article.id} />
+            ))} </div>
+            ); }
+            }
         </ul>
        <h3>Reasons to use Higher-Order component:</h3>
        <ul type="disc">
@@ -22,6 +42,13 @@ function App() {
         <li>PureComponent is Primarily Used for Performance Optimization</li>
         <li>We can use React.memo for transforming a component into pure component</li>
        </ul>
+       // Example of Pure Components :--
+          import React from ‘react’;
+          export default class Test extends React.PureComponent{
+          render(){
+          return <h1>Welcome to GeeksforGeeks</h1>;
+    }
+   }
     
     </div>
   );
